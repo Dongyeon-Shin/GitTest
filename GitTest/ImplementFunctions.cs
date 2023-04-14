9acb12e7a34e14131a5bad283c6de7dd9af3d114
DongyeonShin <d.shin0920@gmail.com>
2023-04-14 20:13:53 +0900

    feat: 코딩 3번 문제 기능 구현
    매개변수가 소수인지 판별하는 함수

--- GitTest/ImplementFunctions.cs
+++ GitTest/ImplementFunctions.cs
@@ -28,6 +28,17 @@ namespace GitTest
             }
             Console.WriteLine(count);
         }
+        static bool IsPrime(int n)
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
 
     }
 }
