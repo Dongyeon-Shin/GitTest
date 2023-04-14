ccda6b209088aa46a283edd5c5081c93740f6a07
DongyeonShin <d.shin0920@gmail.com>
2023-04-14 20:15:49 +0900

    feat: 코딩 4번 문제 기능 구현
    매개변수의 각 자리수 숫자들의 합을 구하는 함수

--- GitTest/ImplementFunctions.cs
+++ GitTest/ImplementFunctions.cs
@@ -39,6 +39,15 @@ namespace GitTest
             }
             return true;
         }
+        static int SumOfDigits(int num)
+        {
+            int sum = 0;
+            for (int i = num; i > 0; i = i / 10)
+            {
+                sum += i % 10;
+            }
+            return sum;
+        }
 
     }
 }
