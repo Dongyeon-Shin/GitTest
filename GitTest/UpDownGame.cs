40dbe6417d2e4b60e609d5a255218f1892d1bfb1
DongyeonShin <d.shin0920@gmail.com>
2023-04-14 20:37:48 +0900

    feat: 업다운 게임 게임오버와 다시하기 기능 추가
    플레이어에게 10번의 기회를 주고 게임오버시 재도전할지 결정할 수 있게하는 코드

---
+++ GitTest/UpDownGame.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    internal class UpDownGame
+    {
+        public static void StartUpDownGame()
+        {
+            Random random = new Random();
+            int comNumber = random.Next(0, 1000);
+            int guessNumber;
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 9)
+                {
+                    Console.WriteLine("Game Over.");
+                    Console.WriteLine(" Continue? Press Enter.");
+                    if (Console.ReadKey().Key == ConsoleKey.Enter)
+                    {
+                        i = -1;
+                        comNumber = random.Next(0, 1000);
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+            }
+        }
+
+    }
+}
