3b850db1a41019109d94eaa6103680068c905a81
DongyeonShin <d.shin0920@gmail.com>
2023-04-14 19:52:06 +0900

    feat: 코딩 1번 문제 기능 구현
    문장과 단어를 입력받고 단어가 그 문장의 몇번쨰 인덱스에 포함되어 있는지 찾는 함수

---
+++ GitTest/ImplementFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    internal class ImplementFunctions
+    {
+        static void FindWord()
+        {
+            string sentence = Console.ReadLine();
+            string word = Console.ReadLine(); ;
+            Console.WriteLine(sentence.LastIndexOf(word));
+        }
+
+    }
+}
