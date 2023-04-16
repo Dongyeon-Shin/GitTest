9be51e99ec030fc9ac9c9301ee78ca5340f26529
Dongyeon Shin <d.shin0920@gmail.com>
2023-04-17 02:50:58 +0900

    feat: Player 클래스와 게임 실행 함수 구현
    키 입력을 받고 움직일 수 있으면 움직이는 코드와 게임을 실행하는 함수

--- GitTest/SlidePuzzleGame.cs
+++ GitTest/SlidePuzzleGame.cs
@@ -76,4 +76,92 @@ namespace GitTest
             }
         }
     }
+    public class Player
+    {
+        enum Input
+        {
+            None,
+            Left,
+            Right,
+            Up,
+            Down
+        }
+        ConsoleKeyInfo info;
+        Input input;
+        public void GetKeyInput()
+        {
+            info = Console.ReadKey();
+            switch (info.Key)
+            {
+                case ConsoleKey.LeftArrow:
+                    input = Input.Left;
+                    break;
+                case ConsoleKey.RightArrow:
+                    input = Input.Right;
+                    break;
+                case ConsoleKey.UpArrow:
+                    input = Input.Up;
+                    break;
+                case ConsoleKey.DownArrow:
+                    input = Input.Down;
+                    break;
+                default:
+                    input = Input.None;
+                    break;
+            }
+        }
+        public void Move(Puzzle puzzle)
+        {
+            int prevPlayerPosition = puzzle.PlayerPosition;
+            switch(input)
+            {
+                case Input.Left:
+                    if (prevPlayerPosition % 5 != 0)
+                    {
+                        puzzle.PlayerPosition -= 1;
+                    }
+                    break;
+                case Input.Right:
+                    if (prevPlayerPosition % 5 != 4)
+                    {
+                        puzzle.PlayerPosition += 1;
+                    }
+                    break;
+                case Input.Up:
+                    if (prevPlayerPosition > 4)
+                    {
+                        puzzle.PlayerPosition -= 5;
+                    }
+                    break;
+                case Input.Down:
+                    if (prevPlayerPosition < 20)
+                    {
+                        puzzle.PlayerPosition += 5;
+                    }
+                    break;
+                default:
+                    {
+                        break;
+                    }
+            }
+            puzzle.PuzzleBoard[prevPlayerPosition] = puzzle.PuzzleBoard[puzzle.PlayerPosition];
+            puzzle.PuzzleBoard[puzzle.PlayerPosition] = 0;
+        }
+    }
+    internal class SlidePuzzleGame
+    {
+        public void Play()
+        {
+            Puzzle puzzle = new Puzzle();
+            Player player = new Player();
+            puzzle.Shuffle();
+            puzzle.PrintPuzzleBoard();
+            while (true)
+            {
+                player.GetKeyInput();
+                player.Move(puzzle);
+                puzzle.PrintPuzzleBoard();
+            }
+        }
+    }
 }
