c4845a80045357c4e4a58cb3e5f4cf3cf94bf760
Dongyeon Shin <d.shin0920@gmail.com>
2023-04-17 10:08:46 +0900

    feat: 빙고 게임 완성
    중복하지 않은 랜덤한 숫자들로 빙고판을 구성하고 플레이어가 그 중 하나의 숫자를 입력할때마다 체크하고 빙고를 체크하는 기능

---
+++ GitTest/BingoGame.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    internal class BingoGame
+    {
+        public static void PlayBingoGame()
+        {
+            int[] bingoBoard = GetRandomNumbers(25);
+            int lastSelectedPosition;
+            while (true)
+            {
+                PringBingoBoard(bingoBoard);
+                lastSelectedPosition = GetInput(bingoBoard);
+                if (CheckResult(bingoBoard, lastSelectedPosition))
+                {
+                    Console.WriteLine("Bingo");
+                    break;
+                }
+            }
+        }
+        static int GetInput(int[] bingoBoard)
+        {
+            int selectedNumber;
+            Console.WriteLine("Select One Number.");
+            while (!int.TryParse(Console.ReadLine(), out selectedNumber) || Array.IndexOf(bingoBoard, selectedNumber) == -1)
+            {
+                Console.WriteLine("Wrong Value.");
+            }
+            selectedNumber = Array.IndexOf(bingoBoard, selectedNumber);
+            bingoBoard[selectedNumber] = -1;
+            return selectedNumber;
+        }
+        static bool CheckResult(int[] bingoBoard, int lastSelectedPosition)
+        {
+            bool isBingo = true;
+            int i = 0;
+            while (true)
+            {
+                if (bingoBoard[i] != -1)
+                {
+                    isBingo = false;
+                    break;
+                }
+                if (i % 5 == 0)
+                {
+                    break;
+                }
+                i--;
+            }
+            i = 0;
+            while (true)
+            {
+                if (bingoBoard[i] != -1)
+                {
+                    isBingo = false;
+                    break;
+                }
+                if (i % 5 == 4)
+                {
+                    break;
+                }
+                i++;
+            }
+            if (isBingo)
+            {
+                return isBingo;
+            }
+            isBingo = true;
+            i = 0;
+            while (true)
+            {
+                if (bingoBoard[i] != -1)
+                {
+                    isBingo = false;
+                    break;
+                }
+                i -= 5;
+                if (i < 5)
+                {
+                    break;
+                }
+            }
+            i = 0;
+            while (true)
+            {
+                if (bingoBoard[i] != -1)
+                {
+                    isBingo = false;
+                    break;
+                }
+                if (i > 19)
+                {
+                    break;
+                }
+                i += 5;
+            }
+            if (isBingo)
+            {
+                return isBingo;
+            }
+            isBingo = true;
+            for (i = 0; i < bingoBoard.Length; i++)
+            {
+                if (i % 6 == 0)
+                {
+                    if (bingoBoard[i] != -1)
+                    {
+                        isBingo = false;
+                        break;
+                    }
+                }
+            }
+            if (isBingo)
+            {
+                return isBingo;
+            }
+            isBingo = true;
+            for (i = 0; i < bingoBoard.Length; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    if (bingoBoard[i] != -1)
+                    {
+                        isBingo = false;
+                        break;
+                    }
+                }
+            }
+            return isBingo;
+        }
+        static void PrintBingoBoard(int[] bingoBoard)
+        {
+            Console.Clear();
+            int count = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (bingoBoard[count] == -1)
+                    {
+                        Console.Write(" X               ");
+                    }
+                    else if (bingoBoard[count] < 10)
+                    {
+                        Console.Write(" {0}               ", bingoBoard[count]);
+                    }
+                    else
+                    {
+                        Console.Write(" {0}              ", bingoBoard[count]);
+                    }
+                    count++;
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+        static int[] GetRandomNumbers(int num)
+        {
+            Random random = new Random();
+            int[] randomNumbers = new int[num];
+            randomNumbers[0] = random.Next(0, 50);
+            for (int i = 1; i < num; i++)
+            {
+                randomNumbers[i] = random.Next(0, 50);
+                for (int j = i; j >= 0; j--)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    if (randomNumbers[i] == randomNumbers[j])
+                    {
+                        randomNumbers[i] = random.Next(0, 50);
+                        j = i;
+                    }
+                }
+            }
+            return randomNumbers;
+        }
+
+    }
+}
